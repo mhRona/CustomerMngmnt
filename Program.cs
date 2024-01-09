0ffd953ba234eef2e97fb04e40703d5e8100b7cf
mhRona <44463303+mhRona@users.noreply.github.com>
2024-01-09 21:57:04 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
+using CustomerManagement.Database.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,9 +10,24 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddDbContext<CustomerMngmntContext>(options =>
+    options.UseSqlServer("Data Source=.\\sqlexpress01;Initial Catalog=CustomerMngmnt;Integrated Security=True;TrustServerCertificate=True"));
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("DevelopmentPolicy",
+        builder =>
+        {
+            builder
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+        });
+});
 var app = builder.Build();
 
+
+// Inside Configure method in Startup.cs
+app.UseCors("DevelopmentPolicy");
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
